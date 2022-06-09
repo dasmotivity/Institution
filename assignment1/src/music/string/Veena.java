ee562e253addfa213f05d29ca534016fa5dd07c2
dasmotivity <106802200+dasmotivity@users.noreply.github.com>
2022-06-09 14:17:04 +0530

    Add files via upload

---
+++ assignment1/src/music/string/Veena.java
@@ -0,0 +1,14 @@
+package music.string;
+
+import music.Playable;
+
+public class Veena implements Playable{
+
+
+
+	public void play() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
---
+++ assignment1/src/music/wind/Saxophone.java
@@ -0,0 +1,16 @@
+package music.wind;
+
+import music.Playable;
+
+public class Saxophone implements Playable {
+
+
+	
+
+	public void play() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
