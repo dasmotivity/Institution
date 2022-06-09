62d7ae3c7edcbdaa990cac04fcd2509b41472546
dasmotivity <106802200+dasmotivity@users.noreply.github.com>
2022-06-09 14:19:07 +0530

    Add files via upload

---
+++ assignment3/Calculator.java
@@ -0,0 +1,10 @@
+package assignment3;
+
+public class Calculator {
+	static int powerInt(int numi, int num2) {
+		return (int) Math.pow(numi, num2);
+	}
+	static double powerDouble(double numi, double num2) {
+		return Math.pow(numi, num2);
+	}
+}
---
+++ assignment3/Test.java
@@ -0,0 +1,12 @@
+package assignment3;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Calculator.powerInt(5, 5);
+		Calculator.powerDouble(4.5, 3.2);
+			
+		
+	}
+
+}
